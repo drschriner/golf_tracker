4ea9b013192a4896faf21cbbede97801e4ab2417
drschriner <drschriner@unomaha.edu>
2017-05-17 13:17:18 -0500

    Added the mysql database connection

--- config/connections.js
+++ config/connections.js
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+  golf_tracker_mysql: {
+     adapter: 'sails-mysql',
+     host: 'local_host',
+     user: 'golf_tracker', //optional
+     password: 'Teamusa12!', //optional
+     database: 'golf_tracker' //optional
+   },
 
   /***************************************************************************
   *                                                                          *
