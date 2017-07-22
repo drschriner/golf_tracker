18c597f860f6d093637a069799660efdb71d5147
drschriner <drschriner@unomaha.edu>
2017-07-22 13:39:56 -0500

    Created course API and completed all course information styling

---
+++ api/controllers/CourseController.js
@@ -0,0 +1,74 @@
+/**
+ * CourseController
+ *
+ * @description :: Server-side logic for managing courses
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  'new': function (req, res) {
+    res.view();
+  },
+
+  create: function (req, res, next) {
+    Course.create(req.params.all(), function courseCreated(err, course) {
+      if (err) return next(err);
+
+      res.redirect('/course/show/' + course.id);
+    });
+  },
+
+  show: function (req, res, next) {
+    Course.findOne(req.param('id')).populateAll().exec(function foundCourse(err, course) {
+      if (err) return next(err);
+
+      if (!course) return next();
+
+      res.view({
+        course: course
+      });
+    });
+  },
+
+
+  index: function (req, res, next) {
+    Course.find(function foundCourses(err, courses) {
+      if (err) return next(err);
+
+      res.view({
+        courses: courses
+      });
+    });
+  },
+
+  edit: function (req, res, next) {
+    Course.findOne(req.param('id'), function foundCourse(err, course) {
+      if (err) return next(err);
+
+      if (!course) return next();
+
+      res.view({
+        course: course
+      });
+    });
+  },
+
+  update: function (req, res, next) {
+    Course.update(req.param('id'), req.params.all(), function courseUpdated(err) {
+      if (err) {
+        return res.redirect('/course/edit/' + req.param('id'));
+      }
+
+      res.redirect('/course/show/' + req.param('id'));
+    });
+  },
+
+  destroy: function(req, res, next) {
+    Course.destroy(req.param('id')).exec(function() {
+      res.redirect('/course/');
+    });
+  }
+
+};
+
